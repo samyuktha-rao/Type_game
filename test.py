6626c6f42b263d9cea5ddaab24240928a3495ccd
samyuktha-rao <samyuktha.rao@relanto.ai>
2025-04-15 17:46:18 +0530

    Changes in test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib = [0, 1]
+        for i in range(2, n):
+            fib.append(fib[-1] + fib[-2])
+        return fib
+        
